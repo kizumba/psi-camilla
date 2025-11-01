31c56a5fd0092bc9953f6e208d74bd7a1e830853
kizumba <12.25andre@gmail.com>
2025-11-01 13:38:19 -0300

    Criado página de login

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("auth/login.html")
+    else:
+        return redirect(url_for('home'))
 
 app.run(debug=True)
